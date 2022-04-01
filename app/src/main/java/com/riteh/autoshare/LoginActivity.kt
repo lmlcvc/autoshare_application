8bf592b73568ffdfd2c80ffabd1e84bc92801b23
lmlcvc <lanamilicevic10@gmail.com>
2022-04-01 14:31:12 +0200

    Fixed installation error

--- app/src/main/java/com/riteh/autoshare/LoginActivity.kt
+++ app/src/main/java/com/riteh/autoshare/LoginActivity.kt
@@ -1,13 +1,14 @@
 package com.riteh.autoshare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login)
 
         btnLogin.setOnClickListener {
             performSignUp()
