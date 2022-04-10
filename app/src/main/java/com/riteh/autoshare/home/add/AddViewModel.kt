3cbad6bd1f85601610dc15d9fd80f3776a3aba89
Darijan Jelušić <darijan.jelusic@gmail.com>
2022-04-10 12:41:51 +0200

    Revert "Full search tab location and date range input functionality. Closes #3"

--- app/src/main/java/com/riteh/autoshare/home/add/AddViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.riteh.autoshare.home.add
-
-import androidx.lifecycle.ViewModel
-
-class AddViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/riteh/autoshare/home/info/InfoViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.riteh.autoshare.home.info
-
-import androidx.lifecycle.ViewModel
-
-class InfoViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
