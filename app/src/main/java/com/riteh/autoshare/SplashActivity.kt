7219aca7797578c5c60709aa424827796df222a8
mario_negovetic <mario.negovetic@estudent.hr>
2022-04-21 22:30:21 +0200

    removed border on splah screen

--- app/src/main/java/com/riteh/autoshare/SplashActivity.kt
+++ app/src/main/java/com/riteh/autoshare/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.riteh.autoshare
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -12,23 +13,24 @@ import com.riteh.autoshare.ui.auth.AuthActivity
 import com.riteh.autoshare.ui.home.MainActivity
 
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        removeBorder()
+        newContent()
+    }
 
+    fun removeBorder(){
         @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
                 View.SYSTEM_UI_FLAG_FULLSCREEN or
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        }
-        newContent()
     }
+
     fun newContent(){
         Handler(Looper.getMainLooper()).postDelayed(Runnable {
             val homeIntent = Intent(this, AuthActivity::class.java)
