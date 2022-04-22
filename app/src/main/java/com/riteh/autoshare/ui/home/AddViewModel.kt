da8b04e6bae45a7136af6cfacafd528df91e73e0
Lana Miličević <59740928+lmlcvc@users.noreply.github.com>
2022-04-22 18:31:30 +0200

    Cleanup and other minor fixes

--- app/src/main/java/com/riteh/autoshare/ui/home/AddViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.riteh.autoshare.ui.home
-
-import androidx.lifecycle.ViewModel
-
-class AddViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/riteh/autoshare/ui/home/SearchViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.riteh.autoshare.ui.home
-
-import androidx.lifecycle.ViewModel
-
-class SearchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
