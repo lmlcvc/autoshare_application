5cfb3a275ce4c0f465f1eb26c5cc49d25ee27fd8
Sara Majetic <saramajeticc@gmail.com>
2022-04-26 11:43:08 +0200

    Add user update activity & layout

---
+++ app/src/main/java/com/riteh/autoshare/ui/user/UserUpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.riteh.autoshare.ui.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riteh.autoshare.R
+
+class UserUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_update)
+    }
+}
