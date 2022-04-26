10e06b456505987c227becd85bad1c17a07a2489
mario_negovetic <mario.negovetic@estudent.hr>
2022-04-26 13:47:47 +0200

    added fuel manifest and button in user fragment

--- app/src/main/java/com/riteh/autoshare/ui/home/user/UserFragment.kt
+++ app/src/main/java/com/riteh/autoshare/ui/home/user/UserFragment.kt
@@ -1,12 +1,15 @@
 package com.riteh.autoshare.ui.home.user
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.riteh.autoshare.R
+import com.riteh.autoshare.ui.user.UserCardInfoActivity
 
 class UserFragment : Fragment() {
 
@@ -25,6 +28,12 @@ class UserFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val btnCard = view.findViewById(R.id.button3) as Button
+        btnCard.setOnClickListener{
+            val intent = Intent(it.context, UserCardInfoActivity::class.java)
+            startActivity(intent)
+        }
         viewModel = ViewModelProvider(this)[UserViewModel::class.java]
     }
 
