97e744b608b6464fc9d14854460edaaa6178a5dd
mario_negovetic <mario.negovetic@estudent.hr>
2022-05-18 20:59:29 +0200

    added car layout in search list

---
+++ app/src/main/java/com/riteh/autoshare/ui/home/search/CarsListActivity.kt
@@ -0,0 +1,12 @@
+package com.riteh.autoshare.ui.home.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.riteh.autoshare.R
+
+class CarsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cars_list)
+    }
+}
