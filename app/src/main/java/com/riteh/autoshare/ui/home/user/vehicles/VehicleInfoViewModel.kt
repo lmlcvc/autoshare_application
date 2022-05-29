8860f87295b38730f8c4a4ab1fa5cc6ee3d30830
lana <lanamilicevic10@gmail.com>
2022-05-29 23:13:18 +0200

    Adding all basic vehicle features

--- app/src/main/java/com/riteh/autoshare/ui/home/user/vehicles/VehicleInfoViewModel.kt
+++ app/src/main/java/com/riteh/autoshare/ui/home/user/vehicles/VehicleInfoViewModel.kt
@@ -1,25 +1,11 @@
 package com.riteh.autoshare.ui.home.user.vehicles
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.riteh.autoshare.data.UserPreferences
 import com.riteh.autoshare.data.dataholders.Vehicle
-import com.riteh.autoshare.network.AuthApi
-import com.riteh.autoshare.network.RemoteDataSource
-import com.riteh.autoshare.network.VehicleCreateApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.util.*
 
 
 class VehicleInfoViewModel : ViewModel() {
-
-    private val api = RemoteDataSource().buildApi(VehicleCreateApi::class.java)
 
     val vehicle: MutableLiveData<Vehicle> =
         MutableLiveData<Vehicle>(Vehicle())
@@ -42,39 +28,13 @@ class VehicleInfoViewModel : ViewModel() {
         image: String,
         description: String
     ) {
-        vehicle.value?.seats = seats.toInt()
-        vehicle.value?.doors = doors.toInt()
-        vehicle.value?.year = year.toInt()
+        vehicle.value?.seats = seats
+        vehicle.value?.doors = doors
+        vehicle.value?.year = year
         vehicle.value?.licensePlate = licensePlate
-
-        val formatter = SimpleDateFormat("dd-MM-yyyy", Locale.UK)
-        vehicle.value?.registeredUntil = formatter.parse(registeredUntil)!!
-
+        vehicle.value?.registeredUntil = registeredUntil
         vehicle.value?.image = image
-        vehicle.value?.description = description
-    }
-
-    fun setOwnerID(id: Int) {
-        vehicle.value?.ownerID = id
-    }
-
-    suspend fun createVehicle() {
-        api.createVehicle(
-            vehicle.value?.ownerID!!,
-            vehicle.value?.brand!!,
-            vehicle.value?.model!!,
-            vehicle.value?.year!!,
-            vehicle.value?.seats!!,
-            vehicle.value?.doors!!,
-            vehicle.value?.licensePlate!!,
-            vehicle.value?.registeredUntil!!,
-            vehicle.value?.image!!,
-            vehicle.value?.description!!,
-            vehicle.value?.rentCost!!,
-            vehicle.value?.dailyDistanceLimit!!,
-            vehicle.value?.costPerKilometer!!,
-            vehicle.value?.ratingAvg!!
-        )
+        vehicle.value?.seats = description
     }
 
 }
