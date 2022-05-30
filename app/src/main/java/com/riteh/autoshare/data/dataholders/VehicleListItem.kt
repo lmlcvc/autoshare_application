394061d8129b2dfc4d38f82d20c2534f2d9e7f40
lana <lanamilicevic10@gmail.com>
2022-05-30 09:30:41 +0200

    Add missing files

---
+++ app/src/main/java/com/riteh/autoshare/data/dataholders/VehicleListItem.kt
@@ -0,0 +1,6 @@
+package com.riteh.autoshare.data.dataholders
+
+data class VehicleListItem(
+    val name: String,
+    val year: String
+)
---
+++ app/src/main/java/com/riteh/autoshare/repository/UserRepository.kt
@@ -0,0 +1,20 @@
+package com.riteh.autoshare.repository
+
+import com.riteh.autoshare.network.UserApi
+import java.util.*
+
+class UserRepository(
+    private val api: UserApi,
+) : BaseRepository() {
+
+    suspend fun userUpdate(
+        id: Int,
+        name: String,
+        surname: String,
+        email: String,
+        license_id: String,
+        date_of_birth: Date
+    ) = safeApiCall {
+        api.userUpdate(id, name, surname, email, license_id, date_of_birth)
+    }
+}
