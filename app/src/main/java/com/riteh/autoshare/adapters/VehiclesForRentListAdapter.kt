41ff282ac6bf6c3f57278f114406e8a0efe3f8b0
lmlcvc <lanamilicevic10@gmail.com>
2022-06-02 22:21:28 +0200

    Add vehicle image to list of user's vehicles

--- app/src/main/java/com/riteh/autoshare/adapters/VehiclesForRentListAdapter.kt
+++ app/src/main/java/com/riteh/autoshare/adapters/VehiclesForRentListAdapter.kt
@@ -2,7 +2,9 @@ package com.riteh.autoshare.adapters
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +12,12 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.riteh.autoshare.R
 import com.riteh.autoshare.data.dataholders.Vehicle
 import com.riteh.autoshare.ui.home.add.AddViewModel
 import kotlinx.android.synthetic.main.user_vehicle_items_layout.view.*
+
 
 class VehiclesForRentListAdapter(
     private var vehicles: List<Vehicle>,
@@ -37,9 +41,17 @@ class VehiclesForRentListAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.name.text = vehicles[position].brand + " " + vehicles[position].model
 
-        /*Glide.with(holder.icon)
-            .load(products[position].image)
-            .into(holder.icon)*/
+        if(!vehicles[position].image.isNullOrEmpty()) {
+            Glide.with(holder.image)
+                .load(getBitmapFromBase64(vehicles[position].image))
+                .into(holder.image)
+        }
+    }
+
+
+    private fun getBitmapFromBase64(encodedImage: String): Bitmap {
+        val decodedString: ByteArray = Base64.decode(encodedImage, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
     }
 
 
