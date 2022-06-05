b248b2cf6dfa0ccb8844015b72255ec0629325d9
lmlcvc <lanamilicevic10@gmail.com>
2022-06-05 15:04:15 +0200

    Show vehicle names and images in 'My vehicles'

--- app/src/main/java/com/riteh/autoshare/adapters/VehicleListAdapter.kt
+++ app/src/main/java/com/riteh/autoshare/adapters/VehicleListAdapter.kt
@@ -1,38 +1,52 @@
 package com.riteh.autoshare.adapters
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.riteh.autoshare.R
-import com.riteh.autoshare.data.dataholders.VehicleListItem
+import com.riteh.autoshare.data.dataholders.Vehicle
 import kotlinx.android.synthetic.main.activity_vehicles_list.view.tv_title
 import kotlinx.android.synthetic.main.user_vehicle_items_layout.view.*
 
-class VehicleListAdapter(private var vehicles: List<VehicleListItem>, val context: Context) :
+class VehicleListAdapter(private var vehicles: List<Vehicle>, val context: Context) :
     RecyclerView.Adapter<VehicleListAdapter.ViewHolder>() {
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.user_vehicle_items_layout, parent, false)
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.user_vehicle_items_layout, parent, false)
         return ViewHolder(v)
     }
 
+
     override fun getItemCount(): Int {
-        // return vehicles.size
-        return 7
+        return vehicles.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // dummy values
-        holder.name.text = "vehicle name"
 
-        /*Glide.with(holder.icon)
-            .load(products[position].image)
-            .into(holder.icon)*/
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name.text = vehicles[position].brand + " " + vehicles[position].model
+
+        if(!vehicles[position].image.isNullOrEmpty()) {
+            Glide.with(holder.image)
+                .load(getBitmapFromBase64(vehicles[position].image))
+                .into(holder.image)
+        }
+    }
+
+
+    private fun getBitmapFromBase64(encodedImage: String): Bitmap {
+        val decodedString: ByteArray = Base64.decode(encodedImage, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
     }
 
 
--- app/src/main/java/com/riteh/autoshare/ui/home/user/vehicles/VehicleListActivity.kt
+++ app/src/main/java/com/riteh/autoshare/ui/home/user/vehicles/VehicleListActivity.kt
@@ -6,40 +6,94 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.riteh.autoshare.R
 import com.riteh.autoshare.adapters.VehicleListAdapter
-import com.riteh.autoshare.data.dataholders.VehicleListItem
+import com.riteh.autoshare.data.UserPreferences
+import com.riteh.autoshare.data.dataholders.Vehicle
+import com.riteh.autoshare.network.VehicleApi
 import com.riteh.autoshare.ui.home.MainActivity
 import kotlinx.android.synthetic.main.activity_vehicle_list.*
+import kotlinx.android.synthetic.main.activity_vehicle_list.rv_vehicles
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class VehicleListActivity : AppCompatActivity() {
-    private var vehiclesList = mutableListOf<VehicleListItem>()
+
+    private lateinit var adapter: VehicleListAdapter
+    private var vehiclesList = mutableListOf<Vehicle>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vehicle_list)
 
-        setUpRecyclerView()
+        makeAPIRequestWithOwnerID()
         setOnClickListeners()
     }
 
 
     override fun onResume() {
         super.onResume()
-        setUpRecyclerView()
+        setUpRecyclerView(vehiclesList)
     }
 
 
-    private fun setUpRecyclerView() {
-        /*val prefs = getSharedPreferences("USER_CARD_PREFERENCES", Context.MODE_PRIVATE)
-        val gson = Gson()
-        val json: String? = prefs.getString("userCard", "{}")
-        val userCard: UserCard = gson.fromJson(json, UserCard::class.java)*/
+    private fun makeAPIRequestWithOwnerID() {
+        val userPreferences = UserPreferences(this)
 
+        GlobalScope.launch(
+            Dispatchers.IO
+        ) {
+            userPreferences.getUserFromDataStore().catch { e ->
+                e.printStackTrace()
+            }.collect {
+                withContext(Dispatchers.Main) {
+                    makeAPIRequest(it.id)
+                }
+            }
+        }
+    }
+
+
+    private fun makeAPIRequest(id: Int) {
+        val api: VehicleApi = Retrofit.Builder()
+            .baseUrl(getString(R.string.AUTOSHARE_API))
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(VehicleApi::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val response = api.getVehiclesByUserId(id)
+
+                for (item in response.data) {
+                    vehiclesList.add(item)
+                }
+                withContext(Dispatchers.Main) {
+                    setUpRecyclerView(vehiclesList)
+                }
+            } catch (e: Exception) {
+                println(e.toString())
+            }
+        }
+    }
+
+
+    /**
+     * Set up recyclerview with list of user's vehicles.
+     */
+    private fun setUpRecyclerView(vehicles: MutableList<Vehicle>) {
         rv_vehicles.layoutManager = GridLayoutManager(this, 1)
-        rv_vehicles.adapter = VehicleListAdapter(vehiclesList, this)
 
-        /*val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
-        itemTouchHelper.attachToRecyclerView(rv_cards)*/
+        adapter = VehicleListAdapter(vehicles, this)
+        rv_vehicles.adapter = adapter
     }
+
 
     private fun setOnClickListeners() {
         arrow_back.setOnClickListener {
@@ -56,5 +110,6 @@ class VehicleListActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)    }
+        startActivity(intent)
+    }
 }
