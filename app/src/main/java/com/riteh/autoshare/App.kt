25d6de93bb75f1499944c0b4c32063ee91430fc1
lmlcvc <lanamilicevic10@gmail.com>
2022-06-13 21:47:13 +0200

    Access Autoshare API string from local properties

--- app/src/main/java/com/riteh/autoshare/App.kt
+++ app/src/main/java/com/riteh/autoshare/App.kt
@@ -1,13 +1,21 @@
 package com.riteh.autoshare
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Context
 import com.androidisland.vita.startVita
-import org.koin.core.context.startKoin
-import org.koin.dsl.module
 
 class App : Application() {
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        var context: Context? = null
+    }
+
     override fun onCreate() {
         super.onCreate()
+
         startVita()
+        context = applicationContext
     }
 }
--- app/src/main/java/com/riteh/autoshare/network/RemoteDataSource.kt
+++ app/src/main/java/com/riteh/autoshare/network/RemoteDataSource.kt
@@ -1,24 +1,27 @@
 package com.riteh.autoshare.network
 
+import com.riteh.autoshare.App
 import com.riteh.autoshare.BuildConfig
+import com.riteh.autoshare.R
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RemoteDataSource {
-    companion object{
-        private  const val BASE_URL = "http://46.101.208.185/api/"
+
+    companion object {
+        private val BASE_URL = App.context!!.getString(R.string.AUTOSHARE_API)
     }
 
-    fun<Api> buildApi(
+    fun <Api> buildApi(
         api: Class<Api>
-    ) : Api {
+    ): Api {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(
                 OkHttpClient.Builder().also { client ->
-                    if(BuildConfig.DEBUG){
+                    if (BuildConfig.DEBUG) {
                         val logging = HttpLoggingInterceptor()
                         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
                         client.addInterceptor(logging)
